70c3f6610ef8881ebe6041f5ab37e5b106733584
SzollosiDaniel <144780816+SzollosiDaniel@users.noreply.github.com>
2026-03-21 19:54:56 +0100

    Add files via upload

---
+++ Road.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Zoo_tycoon
+{
+    internal class Road
+    {
+        public Point Point { get; set; }
+        public bool connectedToEntrance { get; set; }
+    }
+}
